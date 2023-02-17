b3ab83feadb5a89d1e040d13242a73be38f79c1f
Elena Kovalevskaya <elena.kovalevskaya6@gmail.com>
2023-02-17 20:54:41 +0300

    add loading and error menu

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,7 +1,9 @@
-import { type FC, useEffect, useRef, useState } from 'react';
+import React, { type FC, useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useLocation, useParams } from 'react-router-dom';
 import cn from 'classnames';
+import { ErrorAlert } from 'components/common/error';
+import { Loader } from 'components/common/loader';
 import { ReactComponent as ArrowIcon } from 'images/arrow_button.svg';
 import { actionsCategories, selectorsCategories } from 'store/menu';
 import type { AppDispatch } from 'store/store';
@@ -28,6 +30,9 @@ export const Menu: FC<MenuProps> = ({ className, testId, testBooksId, testTermsI
 
     const items = useSelector(selectorsCategories.getCategories);
     const dispatch = useDispatch<AppDispatch>();
+    const loading = useSelector(selectorsCategories.getIsLoading);
+    const error = useSelector(selectorsCategories.getIsError);
+    const loaded = useSelector(selectorsCategories.getIsLoaded);
 
     useEffect(() => {
         dispatch(actionsCategories.getCategoriesThunk())
@@ -53,31 +58,38 @@ export const Menu: FC<MenuProps> = ({ className, testId, testBooksId, testTermsI
     }
 
     return (
-        <div className={cn(css.menu, className)}>
-            <div>
-                <div className={cn(css.allBooksLink, { [css.linkActiveUnderline]: isMenuOpen })} data-test-id={testId} onKeyDown={onKeyDown} onClick={onToggleButton} role="button" tabIndex={0}>
-                    <div>Витрина книг</div>
-                    <div className={cn(css.arrow, { [css.arrowOpen]: isMenuOpen })}>
-                        <ArrowIcon className={cn({ [css.arrowActive]: isMenuOpen })} />
-                    </div>
-                </div>
-                <div className={cn(css.navigation)} style={{ height }}>
-                    <ul className={css.list} ref={listRef}>
-                        <li className={css.item}>
-                            <Link onClick={onLinkClick} to="/" data-test-id={testBooksId} className={cn(css.link, { [css.linkActive]: pathname === '/' })}>Все книги</Link>
-                        </li>
-                        {items.map((item) =>
-                            <li className={css.item} key={item.id}>
-                                <Link onClick={onLinkClick} to={`/books/${item.path}`} className={cn(css.link, { [css.linkActive]: category === item.path })}>
-                                    {item.name}<span className={css.count}>{item.count}</span>
-                                </Link>
-                            </li>)}
-                    </ul>
-                </div>
+        <React.Fragment>
+            {loading && <Loader />}
+            {error && <ErrorAlert />}
+            {loaded &&
+                <div className={cn(css.menu, className)}>
+                    <div>
+                        <div className={cn(css.allBooksLink, { [css.linkActiveUnderline]: isMenuOpen })} data-test-id={testId} onKeyDown={onKeyDown} onClick={onToggleButton} role="button" tabIndex={0}>
+                            <div>Витрина книг</div>
+                            <div className={cn(css.arrow, { [css.arrowOpen]: isMenuOpen })}>
+                                <ArrowIcon className={cn({ [css.arrowActive]: isMenuOpen })} />
+                            </div>
+                        </div>
+                        <div className={cn(css.navigation)} style={{ height }}>
+                            <ul className={css.list} ref={listRef}>
+                                <li className={css.item}>
+                                    <Link onClick={onLinkClick} to="/" data-test-id={testBooksId} className={cn(css.link, { [css.linkActive]: pathname === '/' })}>Все книги</Link>
+                                </li>
+                                {items.map((item) =>
+                                    <li className={css.item} key={item.id}>
+                                        <Link onClick={onLinkClick} to={`/books/${item.path}`} className={cn(css.link, { [css.linkActive]: category === item.path })}>
+                                            {item.name}<span className={css.count}>{item.count}</span>
+                                        </Link>
+                                    </li>)}
+                            </ul>
+                        </div>
 
-            </div>
-            <Link to='/terms' onClick={onLinkClick} data-test-id={testTermsId} className={cn(css.link, css.linkBold, { [css.linkActiveUnderline]: pathname === '/terms' })} >Правила пользования</Link>
-            <Link to='/contract' onClick={onLinkClick} data-test-id={testContractId} className={cn(css.link, css.linkBold, { [css.linkActiveUnderline]: pathname === '/contract' })}>Договор оферты</Link>
-        </div>
+                    </div>
+                    <Link to='/terms' onClick={onLinkClick} data-test-id={testTermsId} className={cn(css.link, css.linkBold, { [css.linkActiveUnderline]: pathname === '/terms' })} >Правила пользования</Link>
+                    <Link to='/contract' onClick={onLinkClick} data-test-id={testContractId} className={cn(css.link, css.linkBold, { [css.linkActiveUnderline]: pathname === '/contract' })}>Договор оферты</Link>
+                </div>
+            }
+
+        </React.Fragment>
     );
 }
